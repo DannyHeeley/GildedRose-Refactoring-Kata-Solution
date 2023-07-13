d643a6767cb4bea4c21095ecbe14b8849e1b76c2
Danny Heeley <danny.heeley@bjss.com>
2023-07-13 16:45:06 +0100

    more tests passing and way cleaner

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,60 +11,53 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
             updateItemsBasedOnName(i, item);
+            reduceSellInOfitemBy(1, items, i);
+
         }
+
     }
 
     private void updateItemsBasedOnName(int i, Item item) {
-
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-
-            item.sellIn = item.sellIn - 1;
-
-        }
-
+        //Any item but special cases is reduced by one
         if (!item.name.equals("Aged Brie")
             && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")
             && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-
                 reduceQualityBy(1, items, i);
 
         } else {
-            increaseQualityBy(1, items, i);
-            handleBackstagePasses(i, item);
+            handleVariableQualityBasedOnSellIn(i, item);
         }
 
-        if (!item.name.equals("Aged Brie")) {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    reduceQualityBy(1, items, i);
-                }
-            }
-        } else {
-            increaseQualityBy(1, items, i);
-        }
     }
 
-    private void handleBackstagePasses(int i, Item item) {
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+    private void handleVariableQualityBasedOnSellIn(int i, Item item) {
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")
+            || item.name.equals("Aged Brie")) {
             if (item.sellIn < 11 && item.sellIn > 5) {
-                increaseQualityBy(1, items, i);
+                increaseQualityBy(2, items, i);
             }
             if (item.sellIn < 6) {
-                increaseQualityBy(1, items, i);
+                increaseQualityBy(3, items, i);
             }
+        }
+
+    }
+
+    private void reduceQualityBy(int amount, Item[] items, int i) {
+        if (items[i].quality <= 50) {
+            items[i].quality = items[i].quality - amount;
         }
     }
 
-    private int reduceQualityBy(int amount, Item[] items, int i) {
+    private void increaseQualityBy(int amount, Item[] items, int i) {
         if (items[i].quality <= 50) {
-            return items[i].quality = items[i].quality - amount;
-        } else return items[i].quality;
+            items[i].quality = items[i].quality + amount;
+        }
     }
 
-    private int increaseQualityBy(int amount, Item[] items, int i) {
-        if (items[i].quality <= 50) {
-            return items[i].quality = items[i].quality + amount;
-        } else return items[i].quality;
+    private void reduceSellInOfitemBy(int amount, Item[] items, int i) {
+        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            items[i].sellIn = items[i].sellIn - amount;
+        }
     }
-
 }
