a8098da7524f5be65d343b4067a9b600a16576b2
Danny Heeley <danny.heeley@bjss.com>
2023-07-13 13:56:59 +0100

    Tests writen, most currently failing but tests correct

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -43,10 +43,9 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                        if (items[i].quality > 0
+                            && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
-                            }
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,269 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void itemsHaveNameValue() {
+        Item[] items = new Item[]{new Item("foo", 10, 0)};
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void itemsHaveQualityValue() {
+        Item[] items = new Item[]{new Item("foo", 0, 10)};
+        GildedRose app = new GildedRose(items);
+        assertEquals(10, app.items[0].quality);
+    }
+
+    @Test
+    void itemsHaveSellInValue() {
+        Item[] items = new Item[]{new Item("foo", 10, 0)};
+        GildedRose app = new GildedRose(items);
+        assertEquals(10, app.items[0].sellIn);
+    }
+
+    @Test
+    void itemsHaveAllThreeValues() {
+        Item[] items = new Item[]{new Item("foo", 10, 0)};
+        GildedRose app = new GildedRose(items);
+        assertAll(
+            () -> assertEquals("foo", app.items[0].name),
+            () -> assertEquals(10, app.items[0].sellIn),
+            () -> assertEquals(0, app.items[0].quality)
+        );
+    }
+
+    @Test
+    void itemQualityIsLoweredAtEndOfDay() {
+        Item[] items = new Item[]{new Item("foo", 1, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].quality);
+    }
+
+    @Test
+    void itemSellInIsLoweredAtEndOfDay() {
+        Item[] items = new Item[]{new Item("foo", 1, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].sellIn);
+    }
+
+    @Test
+    void sellInAndQualityAreLoweredAtEndOfDay() {
+        Item[] items = new Item[]{new Item("foo", 10, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(9, app.items[0].sellIn),
+            () -> assertEquals(9, app.items[0].quality)
+        );
+    }
+
+    @Test
+    void qualityDegradesTwiceAsFastIfSellInIs0() {
+        Item[] items = new Item[]{new Item("foo", 0, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void itemQualityIsNeverMinusValue() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void agedItemQualityIsNeverMinusValue() {
+        Item[] items = new Item[]{new Item("Aged Brie", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(0, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Aged"))
+        );
+    }
+
+    @Test
+    void agedQualityIncreasesEachDay() {
+        Item[] items = new Item[]{new Item("Aged Brie", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertTrue(app.items[0].quality > 0),
+            () -> assertTrue(app.items[0].name.contains("Aged"))
+        );
+    }
+
+    @Test
+    void agedQualityIncreasesBy2IfDaysToSellIs6() {
+        Item[] items = new Item[]{new Item("Aged Brie", 6, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(2, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Aged"))
+        );
+    }
+
+    @Test
+    void agedQualityIncreasesBy2IfDaysToSellIs10() {
+        Item[] items = new Item[]{new Item("Aged Brie", 10, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(2, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Aged"))
+        );
+    }
+
+    @Test
+    void agedQualityIncreasesBy3IfDaysToSellIs0() {
+        Item[] items = new Item[]{new Item("Aged Brie", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(3, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Aged"))
+        );
+    }
+
+    @Test
+    void agedQualityIncreasesBy3IfDaysToSellIs5() {
+        Item[] items = new Item[]{new Item("Aged Brie", 5, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(3, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Aged"))
+        );
+    }
+
+    @Test
+    void qualityOfNonAgedItemIsNeverMoreThan50() {
+        Item[] items = new Item[]{new Item("foo", 0, 55)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(49, app.items[0].quality);
+    }
+
+    @Test
+    void qualityOfAgedItemIsNeverMoreThan50() {
+        Item[] items = new Item[]{new Item("Aged Brie", 0, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+        assertAll(
+            () -> assertEquals(50, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Aged"))
+        );
+    }
+
+    @Test
+    public void sulfurasSellInIsAlwaysPositiveValue() {
+        Item[] items = new Item[]{new Item("foo", 0, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertTrue(app.items[0].sellIn > 0),
+            () -> assertTrue(app.items[0].name.contains("Sulfuras"))
+        );
+    }
+
+    @Test
+    public void sulfurasQualityIsAlways80() {
+        Item[] items = new Item[]{new Item("foo", 0, 55)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(80, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Sulfuras"))
+        );
+    }
+
+    @Test
+    public void sulfurasDoesNotDecreaseInQuality() {
+        Item[] items = new Item[]{new Item("foo", 0, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(80, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Sulfuras"))
+        );
+    }
+
+    @Test
+    void backstagePassQualityIncreasesBy2IfDaysToSellIs6() {
+        Item[] items = new Item[]{new Item("Aged Brie", 6, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(2, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Backstage pass"))
+        );
+    }
+
+    @Test
+    void backstagePassQualityIncreasesBy2IfDaysToSellIs10() {
+        Item[] items = new Item[]{new Item("Aged Brie", 10, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(2, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Backstage pass"))
+        );
+    }
+
+    @Test
+    void backstagePassQualityIncreasesBy3IfDaysToSellIs0() {
+        Item[] items = new Item[]{new Item("Aged Brie", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(3, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Backstage pass"))
+        );
+    }
+
+    @Test
+    void backstagePassQualityIncreasesBy3IfDaysToSellIs5() {
+        Item[] items = new Item[]{new Item("Aged Brie", 5, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(3, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Backstage pass"))
+        );
+    }
+
+    @Test
+    public void backstagePassQualityDoesNotExceed50() {
+        Item[] items = new Item[]{new Item("foo", 1, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertAll(
+            () -> assertEquals(50, app.items[0].quality),
+            () -> assertTrue(app.items[0].name.contains("Backstage pass"))
+        );
+    }
+
+//    @Test
+//    public void qualityDropsTo0AfterTheConcert() {
+//        Item[] items = new Item[]{new Item("foo", 0, 55)};
+//        GildedRose app = new GildedRose(items);
+//        app.updateQuality();
+//    }
+
+//    @Test
+//    void conjuredItemsDegradeInQualityTwiceAsFastAsNormalItems() {
+//        Item[] items = new Item[] { new Item("foo", 0, 0) };
+//        GildedRose app = new GildedRose(items);
+//    }
 }
