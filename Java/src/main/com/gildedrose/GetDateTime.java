3069be195abff7154a2bc6bf27f479b120ecbf48
Danny Heeley <danny.heeley@bjss.com>
2023-07-19 16:34:20 +0100

    Removed unnecesary gradle build folder

--- Java/src/main/com/gildedrose/GetDateTime.java
+++ Java/src/main/com/gildedrose/GetDateTime.java
@@ -6,7 +6,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class GetDateTime {
-    private LocalTime parseLocalTime() {
+    private LocalTime parseLocalTime()
+    {
         LocalTime localTime = LocalTime.now();
         String textTime = localTime.format(DateTimeFormatter.ofPattern("HH:mm:ss", Locale.ENGLISH));
         return LocalTime.parse(textTime, DateTimeFormatter.ofPattern("HH:mm:ss", Locale.ENGLISH));
