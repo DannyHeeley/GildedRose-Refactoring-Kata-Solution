af020d56dd910cfc332ef89909ec6cfc1563b577
Danny Heeley <danny.heeley@bjss.com>
2023-07-19 10:46:20 +0100

    All Tests pass, fully refactored and implementation of missing requiremenets

---
+++ Java [My Refactoring]/src/main/java/com/gildedrose/Concerts/Concert.java
@@ -0,0 +1,27 @@
+package com.gildedrose.Concerts;
+
+import com.gildedrose.GetDateTime;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Concert {
+    String concertName;
+    LocalDate concertDate;
+    LocalTime concertEndTime;
+
+    public Concert(String concertName, String concertDate, String concertEndTime) {
+        this.concertName = concertName;
+        this.concertEndTime = LocalTime.parse(concertEndTime, DateTimeFormatter.ofPattern("HH:mm:ss", Locale.ENGLISH));
+        this.concertDate = LocalDate.parse(concertDate, DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH));
+    }
+
+    public boolean isConcertOver(GetDateTime getDateTime) {
+        LocalDate currentDate = getDateTime.getLocalDate();
+        LocalTime currentTime = getDateTime.getLocalTime();
+        return currentDate.isAfter(concertDate) || (currentDate.isEqual(concertDate) && currentTime.isAfter(concertEndTime));
+    }
+}
+
---
+++ Java [My Refactoring]/src/main/java/com/gildedrose/Items/UniqueItems.java
@@ -0,0 +1,10 @@
+package com.gildedrose.Items;
+
+public interface UniqueItems {
+    String BACKSTAGE_PASSES = "Backstage passes";
+    String SULFURAS = "Sulfuras";
+    String CONJURED = "Conjured";
+    String AGED = "Aged";
+}
+
+
